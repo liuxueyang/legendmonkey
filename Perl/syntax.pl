f2365c82725536e6127310fee6b45f633a39f66a
liuxueyang <zypz457@sina.com>
2015-10-13 06:51:59 +0800

    add perl code and Java, Ruby directory

--- Perl/syntax.pl
+++ Perl/syntax.pl
@@ -1,10 +1,16 @@
 #!/usr/bin/perl
+$\ = $/;
 
 $rex = qr/my.STRING/is;
 print $rex;
-print "\n";
 
 @fields = split(/\b(?:a|b|c)\b/, "liu a xue b yang c love");
 print join "&", @fields;
 
-   
+$text_to_search = "example text with [foo] and more";
+$search_string = quotemeta "[foo]";
+print "search_string = $search_string";
+
+print "wee" if $text_to_search =~ /$search_string/;
+
+
